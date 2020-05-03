8a99fc8acb5314bc25396207a6179b5c291d2b8f
Pyunghwa Kim <syneffort@gmail.com>
2020-05-04 08:16:35 +0900

    [#1] 환경설정 (Singleton)

---
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static Managers Instance
+    { 
+        get
+        {
+            Init();
+            return s_instance; 
+        } 
+    }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+}
