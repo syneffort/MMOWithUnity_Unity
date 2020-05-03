1456ae695b6e418bc6df4b35fa01cfefa4b73593
Pyunghwa Kim <syneffort@gmail.com>
2020-05-03 19:15:45 +0900

    Initial Commit

---
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 초기화
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
