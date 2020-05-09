a1678f2e2858ee20cc6d807d698dff23b04a4675
Pyunghwa Kim <syneffort@gmail.com>
2020-05-09 10:47:21 +0900

    [#3] Resource Manager

--- 0_Sources/MMO_Unity/Assets/Scripts/Managers/Managers.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -7,7 +7,10 @@ public class Managers : MonoBehaviour
     static Managers s_instance;
 
     InputManager _input = new InputManager();
+    ResourceManager _resource = new ResourceManager();
+
     public static InputManager Input { get { return Instance._input; } }
+    public static ResourceManager Resource { get { return Instance._resource; } }
 
     // Start is called before the first frame update
     void Start()
