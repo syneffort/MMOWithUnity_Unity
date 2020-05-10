3de332284c9bf26cc8d47c480e98f5e6b55a9eb7
Pyunghwa Kim <syneffort@gmail.com>
2020-05-10 18:17:47 +0900

    [#5] Camera#3

--- 0_Sources/MMO_Unity/Assets/Scripts/Managers/InputManager.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers/InputManager.cs
@@ -17,7 +17,7 @@ public class InputManager
 
         if (MouseAction != null)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButton(0))
             {
                 MouseAction.Invoke(Define.MouseEvent.Press);
 
