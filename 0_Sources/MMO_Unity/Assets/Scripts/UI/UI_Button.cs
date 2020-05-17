a2cb004691770cfe764f7a4e5d775fac69f51f2c
Pyunghwa Kim <syneffort@gmail.com>
2020-05-17 12:34:44 +0900

    [#7] UI Automation 1

--- 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -1,18 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Button : MonoBehaviour
 {
-    [SerializeField]
-    Text _text;
+    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+
+    enum Buttons
+    {
+        PointButton,
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText,
+    }
+
+    private void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+
+        Get<Text>((int)Texts.ScoreText).text = "Bind Test";
+    }
+
+    void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects);
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+        }
+    }
+
+    T Get<T>(int idx) where T : UnityEngine.Object
+    {
+        UnityEngine.Object[] objects = null;
+        if (_objects.TryGetValue(typeof(T), out objects) == false)
+            return null;
+
+        return objects[idx] as T;
+    }
 
     int _score = 0;
 
     public void OnButtonClicked()
     {
-        _score++;
-        _text.text = $"점수 : {_score}";
+        //_score++;
+        //_text.text = $"점수 : {_score}";
     }
 }
