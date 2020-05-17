1728970ad1458d6a7f425bd645966ee85b6289cc
Pyunghwa Kim <syneffort@gmail.com>
2020-05-17 13:18:22 +0900

    [#7] UI Automation 2

--- 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
-
     enum Buttons
     {
         PointButton,
@@ -19,41 +17,24 @@ public class UI_Button : MonoBehaviour
         ScoreText,
     }
 
+    enum GameObjects
+    {
+        TestObject,
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
 
-        Get<Text>((int)Texts.ScoreText).text = "Bind Test";
-    }
-
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-        }
-    }
-
-    T Get<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if (_objects.TryGetValue(typeof(T), out objects) == false)
-            return null;
-
-        return objects[idx] as T;
+        GetText((int)Texts.ScoreText).text = "Bind Test";
     }
 
     int _score = 0;
 
     public void OnButtonClicked()
     {
-        //_score++;
-        //_text.text = $"점수 : {_score}";
+        _score++;
     }
 }
