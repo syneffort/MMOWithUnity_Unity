69c05a9d1e14eb659bc0e9783e756499a673fbb9
Pyunghwa Kim <syneffort@gmail.com>
2020-05-17 14:09:35 +0900

    [#7] UI Automation 3

--- 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_Button.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_Button : UI_Base
@@ -22,13 +23,23 @@ public class UI_Button : UI_Base
         TestObject,
     }
 
+    enum Images
+    {
+        ItemIcon,
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
 
         GetText((int)Texts.ScoreText).text = "Bind Test";
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        UI_EventHandler evt = go.GetComponent<UI_EventHandler>();
+        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; });
     }
 
     int _score = 0;
---
+++ 0_Sources/MMO_Unity/Assets/Scripts/UI/UI_EventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_EventHandler : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    public Action<PointerEventData> OnBeginDragHandler = null;
+    public Action<PointerEventData> OnDragHandler = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (OnBeginDragHandler != null)
+            OnBeginDragHandler.Invoke(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (OnDragHandler != null)
+            OnDragHandler.Invoke(eventData);
+    }
+}
