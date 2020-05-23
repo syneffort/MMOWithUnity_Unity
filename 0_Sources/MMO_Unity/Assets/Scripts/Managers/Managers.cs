b416d8fae80b69dd28b0eb352bce25dcc07a1f6c
Pyunghwa Kim <syneffort@gmail.com>
2020-05-23 11:59:47 +0900

    [#8] Scene Manager 2

--- 0_Sources/MMO_Unity/Assets/Scripts/Managers/Managers.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers/Managers.cs
@@ -8,12 +8,13 @@ public class Managers : MonoBehaviour
 
     InputManager _input = new InputManager();
     ResourceManager _resource = new ResourceManager();
+    SceneManagerEx _scene = new SceneManagerEx();
     UIManager _ui = new UIManager();
 
     public static InputManager Input { get { return Instance._input; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
+    public static SceneManagerEx Scene { get { return Instance._scene; } }
     public static UIManager UI { get { return Instance._ui; } }
-
 
     void Start()
     {
