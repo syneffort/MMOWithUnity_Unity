638260a34dcb98fa19280f256d33423b8ecf0d05
Pyunghwa Kim <syneffort@gmail.com>
2020-05-26 08:10:59 +0900

    [#9] Sound Manager 1

--- 0_Sources/MMO_Unity/Assets/Scripts/TestSound.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/TestSound.cs
@@ -13,4 +13,17 @@ public class TestSound : MonoBehaviour
     {
         
     }
+
+    public AudioClip audioClip;
+    public AudioClip audioClip2;
+    private void OnTriggerEnter(Collider other)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.PlayOneShot(audioClip);
+        audio.PlayOneShot(audioClip2);
+
+        float lifTime = Mathf.Max(audioClip.length, audioClip2.length);
+
+        GameObject.Destroy(gameObject, lifTime);
+    }
 }
