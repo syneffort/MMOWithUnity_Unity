e3e83fc8321bc9e1066e2a81437c030987c74979
Pyunghwa Kim <syneffort@gmail.com>
2020-05-28 07:52:37 +0900

    [#10] Pool Manager 2

--- 0_Sources/MMO_Unity/Assets/Scripts/Managers/PoolManager.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers/PoolManager.cs
@@ -1,16 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PoolManager
 {
+    #region Pool
     class Pool
     {
-        public GameObject original { get; private set; }
+        public GameObject Original { get; private set; }
         public Transform Root { get; set; }
 
         Stack<Poolable> _poolStack = new Stack<Poolable>();
+
+        public void Init(GameObject original, int count = 5)
+        {
+            Original = original;
+            Root = new GameObject().transform;
+            Root.name = $"{original.name}_Root";
+
+            for (int i = 0; i < count; i++)
+            {
+                Push(Create());
+            }
+        }
+
+        Poolable Create()
+        {
+            GameObject go = Object.Instantiate<GameObject>(Original);
+            go.name = Original.name;
+            return go.GetOrAddComponent<Poolable>();
+        }
+
+        public void Push(Poolable poolable)
+        {
+            if (poolable == null)
+                return;
+
+            poolable.transform.parent = Root;
+            poolable.gameObject.SetActive(false);
+            poolable.IsUsing = false;
+
+            _poolStack.Push(poolable);
+        }
+
+        public Poolable Pop(Transform parent)
+        {
+            Poolable poolabe;
+
+            if (_poolStack.Count > 0)
+                poolabe = _poolStack.Pop();
+            else
+                poolabe = Create();
+
+            poolabe.gameObject.SetActive(true);
+            poolabe.IsUsing = true;
+            poolabe.transform.parent = parent;
+
+            return poolabe;
+        }
     }
+    #endregion
 
     Dictionary<string, Pool> _pool = new Dictionary<string, Pool>();
     Transform _root;
@@ -24,18 +74,48 @@ public class PoolManager
         }
     }
 
+    public void CreatePool(GameObject original, int count = 5)
+    {
+        Pool pool = new Pool();
+        pool.Init(original, count);
+        pool.Root.parent = _root;
+
+        _pool.Add(original.name, pool);
+    }
+
     public void Push(Poolable poolable)
     {
+        string name = poolable.gameObject.name;
+        if (_pool.ContainsKey(name) == false)
+        {
+            GameObject.Destroy(poolable.gameObject);
+            return;
+        }
 
+        _pool[name].Push(poolable);
     }
 
     public Poolable Pop(GameObject original, Transform parent = null)
     {
-        return null;
+        if (_pool.ContainsKey(original.name) == false)
+            CreatePool(original);
+
+        return _pool[original.name].Pop(parent);
     }
 
     public GameObject GetOriginal(string name)
     {
-        return null;
+        if (_pool.ContainsKey(name) == false)
+            return null;
+
+        return _pool[name].Original;
+    }
+
+    public void Clear()
+    {
+        foreach (Transform child in _root)
+            GameObject.Destroy(child.gameObject);
+
+        _pool.Clear();
     }
 }
