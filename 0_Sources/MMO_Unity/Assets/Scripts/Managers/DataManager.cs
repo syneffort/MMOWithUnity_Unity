e7bd7bf615352fd67e04bfc537e4f399439c082d
Pyunghwa Kim <syneffort@gmail.com>
2020-05-30 12:59:35 +0900

    [#12] Data Manager 1

---
+++ 0_Sources/MMO_Unity/Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Stat
+{
+    public int level;
+    public int hp;
+    public int attack;
+}
+
+[Serializable]
+public class StatData
+{
+    public List<Stat> stats = new List<Stat>();
+}
+
+public class DataManager
+{
+    public void Init()
+    {
+        TextAsset textAsset = Managers.Resource.Load<TextAsset>($"Data/StatData");
+        StatData data = JsonUtility.FromJson<StatData>(textAsset.text);
+
+    }
+}
--- 0_Sources/MMO_Unity/Assets/Scripts/Scences/GameScene.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Scences/GameScene.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
-
     protected override void Init()
     {
         base.Init();
@@ -13,29 +11,6 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        co = StartCoroutine("ExplodeAfterSecond", 4.0f);
-        StartCoroutine("CoStopExlode", 2.0f);
-    }
-
-    IEnumerator ExplodeAfterSecond(float second)
-    {
-        Debug.Log("Explode Enter");
-        yield return new WaitForSeconds(second);
-        Debug.Log("Explode Executed");
-        co = null;
-    }
-
-    IEnumerator CoStopExlode(float second)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(second);
-        Debug.Log("Stop Executed");
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
     }
 
     public override void Clear()
