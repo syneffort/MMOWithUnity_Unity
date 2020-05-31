a8e337815db24e9579e41052f9357f5337d4f111
Pyunghwa Kim <syneffort@gmail.com>
2020-05-31 10:50:41 +0900

    [#13] Move

--- 0_Sources/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -36,14 +37,28 @@ public class PlayerController : MonoBehaviour
     void UpdateMoving()
     {
         Vector3 dir = _destPos - transform.position;
-        if (dir.magnitude < 0.0001f)
+        if (dir.magnitude < 0.1f)
         {
             _state = PlayerState.Idle;
         }
         else
         {
+            // TODO
+            NavMeshAgent nma =  gameObject.GetOrAddComponent<NavMeshAgent>();
             float moveDist = Mathf.Clamp(Time.deltaTime * _speed, 0, dir.magnitude);
-            transform.position += dir.normalized * moveDist;
+            //nma.CalculatePath
+            nma.Move(dir.normalized * moveDist);
+
+            Debug.DrawRay(transform.position + Vector3.up * 0.5f, dir.normalized, Color.red, 1.0f);
+            if (Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+                _state = PlayerState.Idle;
+                return;
+            }
+
+
+            //float moveDist = Mathf.Clamp(Time.deltaTime * _speed, 0, dir.magnitude);
+            //transform.position += dir.normalized * moveDist;
 
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10.0f * Time.deltaTime);
             //transform.LookAt(_destPos);
