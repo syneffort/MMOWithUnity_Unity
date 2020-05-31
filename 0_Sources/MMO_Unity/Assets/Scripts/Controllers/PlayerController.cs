9448ef4a504b435f7e2cf0e2258ddfdca007cbab
Pyunghwa Kim <syneffort@gmail.com>
2020-05-31 12:05:35 +0900

    [#13] Cursor

--- 0_Sources/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ 0_Sources/MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,8 +13,27 @@ public class PlayerController : MonoBehaviour
 
     Vector3 _destPos;
 
+    Texture2D _attackIcon;
+    Texture2D _handIcon;
+    Vector2 _attackPos;
+    Vector2 _handPos;
+
+    enum CursorType
+    {
+        None,
+        Attack,
+        Hand,
+    }
+
+    CursorType _cursorType = CursorType.None;
+
     void Start()
     {
+        _attackIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Attack");
+        _attackPos = new Vector2(_attackIcon.width / 5, 0);
+        _handIcon = Managers.Resource.Load<Texture2D>("Textures/Cursor/Hand");
+        _handPos = new Vector2(_handIcon.width / 3, 0);
+
         _stat = gameObject.GetComponent<PlayerStat>();
 
         //Managers.Input.KeyAction -= OnKeyboard;
@@ -80,6 +101,8 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        UpdateMouseCursor();
+
         switch (_state)
         {
             case PlayerState.Die:
@@ -91,6 +114,33 @@ public class PlayerController : MonoBehaviour
             case PlayerState.Idle:
                 UpdateIdle();
                 break;
+        }
+    }
+
+    void UpdateMouseCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Camera.main.farClipPlane, _mask))
+        {
+            if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+                if (_cursorType != CursorType.Attack)
+                {
+                    Cursor.SetCursor(_attackIcon, _attackPos, CursorMode.Auto);
+                    _cursorType = CursorType.Attack;
+                }
+                   
+            }
+            else
+            {
+                if (_cursorType != CursorType.Hand)
+                {
+                    Cursor.SetCursor(_handIcon, _handPos, CursorMode.Auto);
+                    _cursorType = CursorType.Hand;
+                } 
+            }    
         }
     }
 
